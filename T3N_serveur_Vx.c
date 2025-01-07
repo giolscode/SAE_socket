559ba76e9e9f169412afb6f6935c54e15ca046c9
LOSAT Giovanni <giolstpro@gmail.com>
2025-01-07 13:47:33 +0100

    avec library

---
+++ T3N_serveur_Vx.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h> /* pour exit */
+#include <unistd.h> /* pour read, write, close, sleep */
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <string.h> /* pour memset */
+#include <netinet/in.h> /* pour struct sockaddr_in */
+#include <arpa/inet.h> /* pour htons et inet_aton */
