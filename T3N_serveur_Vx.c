7bbe04695d25411756c4f5a7144a460295c55ba7
LOSAT Giovanni <giolstpro@gmail.com>
2025-01-07 15:06:31 +0100

    Serveur V0.1

--- T3N_serveur_Vx.c
+++ T3N_serveur_Vx.c
@@ -6,3 +6,53 @@
 #include <string.h> /* pour memset */
 #include <netinet/in.h> /* pour struct sockaddr_in */
 #include <arpa/inet.h> /* pour htons et inet_aton */
+
+#define PORT 6000 
+#define LG_MESSAGE 256
+
+int main(int argc, char const *argv[])
+{
+    int socketEcoute;
+
+	struct sockaddr_in pointDeRencontreLocal;
+	socklen_t longueurAdresse;
+
+	int socketDialogue;
+	struct sockaddr_in pointDeRencontreDistant;
+	char messageRecu[LG_MESSAGE]; /* le message de la couche Application ! */
+	int ecrits, lus; /* nb d’octets ecrits et lus */
+	int retour;
+
+    //Création du socket
+    socketEcoute = socket(AF_INET, SOCK_STREAM, 0); 
+    if (socketEcoute < 0){
+        perror("socket");
+        exit(-1);
+    }
+    printf("Socket créée avec succès ! (%d)\n", socketEcoute);
+
+    // Remplissage de sockaddrDistant (structure sockaddr_in identifiant le point d'écoute local)
+	longueurAdresse = sizeof(pointDeRencontreLocal);
+	// memset sert à faire une copie d'un octet n fois à partir d'une adresse mémoire donnée
+	// ici l'octet 0 est recopié longueurAdresse fois à partir de l'adresse &pointDeRencontreLocal
+	memset(&pointDeRencontreLocal, 0x00, longueurAdresse); pointDeRencontreLocal.sin_family = PF_INET;
+	pointDeRencontreLocal.sin_addr.s_addr = htonl(INADDR_ANY); // attaché à toutes les interfaces locales disponibles
+	pointDeRencontreLocal.sin_port = htons(PORT); // = 5000 ou plus
+
+    // On demande l’attachement local de la socket
+	if((bind(socketEcoute, (struct sockaddr *)&pointDeRencontreLocal, longueurAdresse)) < 0) {
+		perror("bind");
+		exit(-2); 
+	}
+	printf("Socket attachée avec succès !\n");
+
+    //en Ecoute
+    if(listen(socketEcoute, 5) < 0){
+   		perror("listen");
+   		exit(-3);
+	}
+	printf("Socket placée en écoute passive ...\n");
+    
+
+    return 0;
+}
